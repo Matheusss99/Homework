72b4c8905518ebcfdf1c4db61d1cd1299b4b0a8c
Matheusss99 <112821758+Matheusss99@users.noreply.github.com>
2022-09-04 21:37:21 +0400

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,65 @@
+from telnetlib import FORWARD_X
+from turtle import *
+
+speed (6)
+
+
+width (3)
+color ("grey")
+forward (220)
+left (90)
+forward (220)
+left (90)
+forward (220)
+left(90)
+forward (220)
+left (90)
+forward (80)
+color ("purple")
+begin_fill ()
+left (90)
+forward (112)
+right (90)
+forward (50)
+right (90)
+forward (113)
+end_fill ()
+penup ()
+goto (220, 220)
+pendown ()
+color ("purple")
+begin_fill()
+forward (1)
+right (154)
+color ("brown")
+forward (260)
+left (130)
+forward (263)
+end_fill()
+color ("brown")
+left (25)
+forward (66)
+left (90)
+forward (70)
+left (90)
+forward (68)
+left (90)
+color ("brown")
+forward (38)
+left (91)
+color ("brown")
+begin_fill
+forward (69)
+left (90)
+forward (35) #35
+left (90)
+forward (37) #37
+left (90)
+forward (70) #77
+penup ()
+goto (220, 220)
+
+
+
+
+
